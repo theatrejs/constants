ea8937b9129b84806d6c865f2eb02f86420131fc
Alexandre Blondeau <deformhead@gmail.com>
2018-05-22 13:37:32 +0200

    update export type

--- sources/constants.js
+++ sources/constants.js
@@ -1,11 +1,19 @@
 // Euler's number (natural exponential e), rounded to the millionth
-export const E = 2.718282
+const E = 2.718282
 
 // gravitational acceleration (m/s²), exact value
-export const G = 9.80665;
+const G = 9.80665;
 
 // Pi, rounded to the millionth
-export const PI = 3.141593;
+const PI = 3.141593;
 
 // square root of 2, rounded to the millionth
-export const SQRT_2 = 1.414214;
+const SQRT_2 = 1.414214;
+
+export {
+
+    E,
+    G,
+    PI,
+    SQRT_2
+};
